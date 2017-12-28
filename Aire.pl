9866c62d7feaac0807ee43a9140e506bb9dcbe10
QUINQUENEL Nicolas <zenntco@gmail.com>
2017-12-28 02:11:29 +0100

    Ajout méthode qui retourne l'aire entre chaque dimension pour chaque individu

--- Aire.pl
+++ Aire.pl
@@ -9,21 +9,35 @@
 % N/D 15/12/2017 Restructuration du projet (jeu de données écrit en dur)
 % 49c0b9c 23/12/2017 Refactorisation du code faite et renvoi de la
 % meilleure permutation possible
+% d3fb99f 24/12/2017 Factorisation code inutile
 
 max(dim1, 10).
 max(dim2, 9).
 max(dim3, 8).
 max(dim4, 20).
+max(dim5, 500).
+max(dim6, 148).
 
 value(diag1, dim1, 5).
 value(diag1, dim2, 1).
 value(diag1, dim3, 6).
 value(diag1, dim4, 16).
+value(diag1, dim5, 471).
+value(diag1, dim6, 34).
 
 value(diag2, dim1, 2).
 value(diag2, dim2, 7).
 value(diag2, dim3, 3).
 value(diag2, dim4, 2).
+value(diag2, dim5, 347).
+value(diag2, dim6, 110).
+
+value(diag3, dim1, 7).
+value(diag3, dim2, 4).
+value(diag3, dim3, 1).
+value(diag3, dim4, 5).
+value(diag3, dim5, 187).
+value(diag3, dim6, 29).
 
 % Calcule l'aire entre 2 caractéristiques
 aire(Diag, [Dim1, Dim2], Tmp, RET):-
@@ -66,9 +80,10 @@ sommeAire([Diag], ListDim, TmpAire, RET):-
 
 % Effectue les permutations et trouve la meilleure permutation (qui
 % maximise la somme des aires des graphes)
-maxAirePermuInit(ListDiag, ListDim, RETPermu):-
+maxAirePermuInit(ListDiag, ListDim, RETPermu, ListTmp):-
     findall(V2, permutation(ListDim, V2), ListDimPermu),
-    maxAirePermu(ListDiag, ListDimPermu, [], 0, RETPermu).
+    maxAirePermu(ListDiag, ListDimPermu, [], 0, RETPermu),
+    sommeAirePosInit(ListDiag, RETPermu, ListTmp).
 
 maxAirePermu(ListDiag, [ListPermu1|ListPermuAutres], TmpPermu, TmpAire, RET):-
     sommeAireInit(ListDiag, ListPermu1, Aire),
@@ -83,6 +98,45 @@ maxAirePermu(ListDiag, [ListPermu1], TmpPermu, TmpAire, RET):-
         RET = ListPermu1
     ;   RET = TmpPermu
     ).
+
+% Calcule l'aire entre 2 caractéristiques
+airePos(Diag, [Dim1, Dim2], TmpArray, RET):-
+    value(Diag, Dim1, X1),
+    value(Diag, Dim2, X2),
+    max(Dim1, Y1),
+    max(Dim2, Y2),
+    Tmp2 is ((1/2*(X1/Y1)*(X2/Y2))),
+    insertEnd(Tmp2, TmpArray, RET).
+
+airePos(Diag, [Dim1, Dim2|ListDim], TmpArray, RET):-
+    value(Diag, Dim1, X1),
+    value(Diag, Dim2, X2),
+    max(Dim1, Y1),
+    max(Dim2, Y2),
+    RET1 is ((1/2*(X1/Y1)*(X2/Y2))),
+    insertEnd(RET1, TmpArray, TmpArray2),
+    airePos(Diag, [Dim2|ListDim], TmpArray2, RET).
+
+airePosInit(Diag,[Dim1|ListDim], RET):-
+    insertEnd(Dim1, [Dim1|ListDim], N1),
+    airePos(Diag, N1, [], RET).
+
+% Calcule la somme des aires de tous les graphes pour une permutation
+sommeAirePosInit(ListDiag, ListDim, RET):-
+    sommeAirePos(ListDiag, ListDim, [], RET).
+
+sommeAirePos([Diag|ListDiag], ListDim, TmpArray, RET):-
+    airePosInit(Diag, ListDim, ListAire),
+    insertEnd(ListAire, TmpArray, TmpArray2),
+    sommeAirePos(ListDiag, ListDim, TmpArray2, RET).
+
+sommeAirePos([Diag], ListDim, TmpArray, RET):-
+    airePosInit(Diag, ListDim, ListAire),
+    insertEnd(ListAire, TmpArray, RET).
+
+
+
+
 
 
 

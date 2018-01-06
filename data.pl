49aa9b5887873d7bdbd20ea1554783746016c0a7
laouenanlc <laouenanlc@gmail.com>
2018-01-07 00:21:46 +0100

    Augmentation du jeu de données

--- data.pl
+++ data.pl
@@ -7,6 +7,9 @@ max(dim3, 8).
 max(dim4, 5).
 max(dim5, 12).
 max(dim6, 2).
+max(dim7, 10).
+max(dim8, 20).
+
 
 % Diagrammes
 % Nom du diagramme, Dimension, Valeur
@@ -17,6 +20,9 @@ value(diag1, dim3, 2).
 value(diag1, dim4, 2).
 value(diag1, dim5, 2).
 value(diag1, dim6, 2).
+value(diag1, dim7, 5).
+value(diag1, dim8, 18).
+
 
 value(diag2, dim1, 3).
 value(diag2, dim2, 5).
@@ -24,6 +30,9 @@ value(diag2, dim3, 8).
 value(diag2, dim4, 3).
 value(diag2, dim5, 3).
 value(diag2, dim6, 1).
+value(diag2, dim7, 8).
+value(diag2, dim8, 4).
+
 
 value(diag3, dim1, 3).
 value(diag3, dim2, 2).
@@ -31,6 +40,10 @@ value(diag3, dim3, 3).
 value(diag3, dim4, 5).
 value(diag3, dim5, 5).
 value(diag3, dim6, 0).
+value(diag3, dim7, 5).
+value(diag3, dim8, 15).
+
+
 
 value(diag4, dim1, 1).
 value(diag4, dim2, 1).
@@ -38,4 +51,68 @@ value(diag4, dim3, 1).
 value(diag4, dim4, 1).
 value(diag4, dim5, 5).
 value(diag4, dim6, 0).
+value(diag4, dim7, 3).
+value(diag4, dim8, 12).
+
+
+value(diag5, dim1, 2).
+value(diag5, dim2, 1).
+value(diag5, dim3, 4).
+value(diag5, dim4, 1).
+value(diag5, dim5, 5).
+value(diag5, dim6, 2).
+value(diag5, dim7, 8).
+value(diag5, dim8, 7).
+
+value(diag6, dim1, 4).
+value(diag6, dim2, 2).
+value(diag6, dim3, 8).
+value(diag6, dim4, 3).
+value(diag6, dim5, 4).
+value(diag6, dim6, 2).
+value(diag6, dim7, 7).
+value(diag6, dim8, 14).
+
+value(diag7, dim1, 3).
+value(diag7, dim2, 2).
+value(diag7, dim3, 6).
+value(diag7, dim4, 1).
+value(diag7, dim5, 10).
+value(diag7, dim6, 1).
+value(diag7, dim7, 3).
+value(diag7, dim8, 19).
+
+
+value(diag8, dim1, 4).
+value(diag8, dim2, 1).
+value(diag8, dim3, 8).
+value(diag8, dim4, 5).
+value(diag8, dim5, 12).
+value(diag8, dim6, 2).
+value(diag8, dim7, 10).
+value(diag8, dim8, 20).
+
+
+value(diag9, dim1, 2).
+value(diag9, dim2, 5).
+value(diag9, dim3, 4).
+value(diag9, dim4, 5).
+value(diag9, dim5, 12).
+value(diag9, dim6, 2).
+value(diag9, dim7, 4).
+value(diag9, dim8, 1).
+
+
+value(diag10, dim1, 2).
+value(diag10, dim2, 4).
+value(diag10, dim3, 8).
+value(diag10, dim4, 1).
+value(diag10, dim5, 6).
+value(diag10, dim6, 1).
+value(diag10, dim7, 6).
+value(diag10, dim8, 17).
+
+
+
+
 
